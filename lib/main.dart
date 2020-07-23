03e8bcba8a56ea63d3284c588a62fbcad2705bd8
srijan-chadha7 <srijan.chadha14@gmail.com>
2020-07-23 18:15:42 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+import 'quiz_brain.dart';
+import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+QuizBrain quizBrain = QuizBrain();
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+  int e;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.getQuestionText(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                if (quizBrain.getAnswer() == true) {
+                  scoreKeeper.add(Icon(
+                    Icons.check,
+                    color: Colors.green,
+                  ));
+                } else {
+                  scoreKeeper.add(Icon(
+                    Icons.close,
+                    color: Colors.red,
+                  ));
+                }
+                setState(() {
+                  quizBrain.nextQuestion();
+                  if ((e = quizBrain.isFinished()) == 1) {
+                    Alert(
+                      context: context,
+                      type: AlertType.error,
+                      title: "Finished",
+                      desc: "You have successfully finished the quiz",
+                      buttons: [
+                        DialogButton(
+                          child: Text(
+                            "Cancel",
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              scoreKeeper.clear();
+                              quizBrain.reset();
+                              Navigator.pop(context);
+                            });
+                          },
+                          width: 120,
+                        )
+                      ],
+                    ).show();
+                  }
+                });
+                //The user picked true.
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                if (quizBrain.getAnswer() == false) {
+                  scoreKeeper.add(Icon(
+                    Icons.check,
+                    color: Colors.green,
+                  ));
+                } else {
+                  scoreKeeper.add(Icon(
+                    Icons.close,
+                    color: Colors.red,
+                  ));
+                }
+                setState(() {
+                  quizBrain.nextQuestion();
+                  if ((e = quizBrain.isFinished()) == 1) {
+                    Alert(
+                      context: context,
+                      type: AlertType.error,
+                      title: "Finished",
+                      desc: "You have successfully finished the quiz",
+                      buttons: [
+                        DialogButton(
+                          child: Text(
+                            "Cancel",
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              scoreKeeper.clear();
+                              quizBrain.reset();
+                              Navigator.pop(context);
+                            });
+                          },
+                          width: 120,
+                        )
+                      ],
+                    ).show();
+                  }
+                });
+                //The user picked false.
+              },
+            ),
+          ),
+        ),
+        Row(
+          children: scoreKeeper,
+        ),
+      ],
+    );
+  }
+}
